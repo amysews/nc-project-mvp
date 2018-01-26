c692971447efe13f0b4424cb21c6cf7ccb59e1bb
Stephen McKeown <stephenmckeown@Stephens-MacBook-Pro.local>
2018-01-26 11:34:44 +0000

    submit question to db, text_in_bucket flag not updating in db

--- mvp-react/src/components/SubmitQuestion.js
+++ mvp-react/src/components/SubmitQuestion.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import _ from 'lodash'
 
 class SubmitQuestion extends React.Component {
     state = {
-        questioners: []
+        questioners: [],
+        submitted: false
     }
 
     componentDidMount() {
@@ -14,27 +16,72 @@ class SubmitQuestion extends React.Component {
             })
     }
 
+    submitDatabaseQuestions = (event) => {
+        event.preventDefault()
+
+        let userArray = event.target.children[1].children
+        userArray = [...userArray]
+        let user_id = _.find(userArray, (user) => {
+            return user.selected
+        })
+        user_id = user_id.value;
+        const questionText = event.target.questionText.value
+        const topic = event.target.topic.value
+        const keywords = topic
+        
+        let text_in_bucket = 1
+        console.log(text_in_bucket)
+        // if (questionText.length > 0){
+        //     text_in_bucket = 1
+        // } else {
+        //     text_in_bucket = 0
+        // }
+
+
+        return fetch('http://localhost:3002/rds/questions', {
+            method: 'PUT',
+            body: JSON.stringify({ user_id, topic, keywords, text_in_bucket }),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+            .then(res => {
+                if (res.status === 201) {
+                    this.setState({ submitted: true });
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+       
+    }
+
+
+
     render() {
-        const { questioners } = this.state;
+        const { questioners, submitted } = this.state;
         return (
             <div>
                 <h1>Submit new Question</h1>
-                <form>
+                <form onSubmit={this.submitDatabaseQuestions}>
                     User:<br/>
                     <select name="user">
                         {questioners.map((questioner, i) => {
-                            return <option key={i} value={questioner.id}>{questioner.first_name} {questioner.surname}</option>
+                            return <option id="user_id" key={i} value={questioner.id}>{questioner.first_name} {questioner.surname}</option>
                         })}
                     </select><br/>
                     Question:<br/>
-                    <textarea name="questionText" /><br/>
+                    <textarea id="questionText" name="questionText" /><br/>
                     Topic:<br/>
-                    <input type="text" name="topic" /><br/>
+                    <input id="topic" type="text" name="topic" /><br/>
                     <button type="submit">Submit</button>
                 </form>
+                <span>{submitted ? 'Question submitted' : null}</span>
             </div>
         )
     }
 }
+
+
 
 export default SubmitQuestion;
